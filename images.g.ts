f2f2a168d09888ad993ff6d98c0f841b804a8240
Fiona <fionacsteeves@gmail.com>
2025-11-09 18:30:01 -0400

    Added duck pond, duck, and the ability for the duck to break the glass.

---
+++ images.g.ts
@@ -0,0 +1,107 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . . . . . . . . . 5 5 5 . . . 
+. . . . . . . . . 5 5 5 5 5 . . 
+. . . . . . . . 5 5 5 1 5 5 5 . 
+. . . . . . . . 5 5 5 f 5 5 4 4 
+. . . . . . . . 5 5 5 5 5 4 4 4 
+. . . . . . . . . 5 5 5 5 4 4 4 
+. . . . . . . . . . 5 5 5 . . . 
+4 4 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+5 5 5 4 4 4 4 4 4 5 5 4 5 5 5 5 
+4 5 5 4 5 5 5 5 5 5 5 5 4 5 5 5 
+5 5 5 4 4 5 5 5 5 5 5 5 4 5 5 5 
+. 5 5 5 4 4 5 5 5 5 5 5 4 5 5 5 
+. 5 5 5 5 4 4 5 5 5 5 4 5 5 5 5 
+. . 5 5 5 5 5 4 5 4 5 5 5 5 5 . 
+. . . 5 5 5 5 5 5 5 5 5 5 5 . . 
+. . . . 5 5 5 5 5 5 5 5 5 . . . 
+`;
+            case "image2":
+            case "myImage0":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f . . . . f . . . . . 
+. . . . . . . . f f . . . . . . 
+. . . . . . . f . . . . . . . . 
+. . . . . . . . f . . . . . . . 
+. . . . . . . f . . f . . . . . 
+. . . . . f . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image3":
+            case "myImage1":return img`
+. . . . . . . . . . . . . . . . 
+. . f . . . . . . . . . . . . . 
+. . . f f . . . . . . . . f . . 
+. . . . . f . . . . . . f . . . 
+. . . . . . f . . . . f f . . . 
+. . . . . . . f . . f . . . . . 
+. . . f . . . f . f f . . . . . 
+. . . . f f . f f . f . . . . . 
+. . . . . . f . f f . f f f . . 
+. . . . . . f . f . f . . . f . 
+. . . . . . f . . . f . . . . . 
+. . . f . f f . . f f f . . . . 
+. . . f . . . . . . . f f . . . 
+. f f . f . . . . . . . f . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image4":
+            case "myImage2":return img`
+f . . . . . f f . . . . . . f f 
+. f . f f f . . . . . . . f f . 
+. . f f . . f . . . f . f f . . 
+. . . . f . . f f . . f f f . . 
+. f . . . f . . f f f f . f f . 
+f f f f f . f . . f . . . . f f 
+f . . . . f f f f . . . f . . f 
+. . . . . . f f f . . f . . . . 
+. f . . . f f f . f f . . . . . 
+. . f f f . f . . . . f f . . . 
+f f f . . . f . . . . f f f f . 
+f . . f . . f f . . . f . . f . 
+f . . f . . f f . . f . . . . f 
+f . . . . . f . . . . . . . . f 
+f . . . . . f f . . . . . . . f 
+f . . . . . . f f f f f . . . f 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
+
+// Auto-generated code. Do not edit.
+namespace myTiles {
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,24 +1,34 @@
-/**
- * This is the main file for your project.
- *
- * Create images, tilemaps, animations, and songs using the
- * asset explorer in VS Code. You can reference those assets
- * using the tagged templates on the assets namespace:
- *
- *     assets.image`myImageName`
- *     assets.tilemap`myTilemapName`
- *     assets.tile`myTileName`
- *     assets.animation`myAnimationName`
- *     assets.song`mySongName`
- *
- * New to MakeCode Arcade? Try creating a new project using one
- * of the templates to learn about Sprites, Tilemaps, Animations,
- * and more! Or check out the reference docs here:
- *
- * https://arcade.makecode.com/reference
- */
+scene.setTileMapLevel(assets.tilemap`level`);
+let duckImage = assets.image`myImage`;
+let duck = sprites.create(duckImage, SpriteKind.Food);
+// bottom, left right, bottom-left, bottom-right
+let glassSprites = [assets.tile`myTile0`, assets.tile`myTile2`, assets.tile`myTile3`, assets.tile`myTile4`, assets.tile`myTile5`];
+let brokenGlassSprites = [assets.image`myImage0`, assets.image`myImage1`, assets.image`myImage2`];
+let crackingSprites = [sprites.create(brokenGlassSprites[0], SpriteKind.Enemy)];
+crackingSprites[0].destroy();
+crackingSprites.pop();
+duck.ay = 100;
+duck.vx = 15;
+duck.setBounceOnWall(true)
 
-game.onUpdate(() => {
-    // Code in this function will run once per frame. MakeCode
-    // Arcade games run at 30 FPS
-});
+scene.onHitWall(SpriteKind.Food, function (sprite: Sprite, location: tiles.Location) {
+    let tempBoolean = false
+    for (let i = 0; i < crackingSprites.length; i++) {
+        let tempSprite = crackingSprites[i];
+        if (tempSprite.tilemapLocation() == location) {
+            let tempNumber = brokenGlassSprites.indexOf(tempSprite.image);
+            if (tempNumber < brokenGlassSprites.length) {
+                tempSprite.setImage(brokenGlassSprites[tempNumber + 1]);
+            }
+            else {
+                tempSprite.destroy();
+                crackingSprites.removeAt(i)
+            }
+            let tempBoolean = true;
+        }
+    } 
+    if (tempBoolean == false) {
+        crackingSprites.push(sprites.create(brokenGlassSprites[0], SpriteKind.Enemy))
+        tiles.placeOnTile(crackingSprites[crackingSprites.length - 1], location);
+    }
+})
